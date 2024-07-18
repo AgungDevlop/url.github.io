4233a9e0d0711e3db5ebd60f8727cb04ede2c2e2
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2024-07-18 23:12:58 +0800

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,1 @@
+(function(d,z,s,c){s.src='//'+d+'/400/'+z;s.onerror=s.onload=E;function E(){c&&c();c=null}try{(document.body||document.documentElement).appendChild(s)}catch(e){E()}})('ofleafeona.com',7748460,document.createElement('script'),_dpxgzz)
